baf50914e46fac9752d88d5c1a392a97bb9c86e5
Ludwring Liccien <ludwring.liccien@gmail.com>
2024-04-20 15:14:35 -0500

    refactor(config): setup module config to make it a global module

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { ConfigService } from './services/config/config.service';
 
+@Global()
 @Module({
   providers: [
     {
