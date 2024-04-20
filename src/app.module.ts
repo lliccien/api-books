1c035ad937e4f982e94d84e8cb299087759bad9c
Ludwring Liccien <ludwring.liccien@gmail.com>
2024-04-20 17:59:36 -0500

    feat(product): add module prducts and product domain

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule],
+  imports: [ConfigModule, DatabaseModule, ProductsModule],
   controllers: [],
   providers: [],
 })
---
+++ src/products/domain/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+  id?: string;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  cover: string;
+  attachments: string[];
+}
