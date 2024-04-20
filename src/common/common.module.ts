b0152d4d37b3e18f0dbcd1d0af34992e8ce8e662
Ludwring Liccien <ludwring.liccien@gmail.com>
2024-04-20 18:38:29 -0500

    feat(common): add MappersService to CommonModule

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,4 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { MappersService } from './application/mappers.service';
 
-@Module({})
+@Global()
+@Module({
+  providers: [MappersService],
+  exports: [MappersService],
+})
 export class CommonModule {}
