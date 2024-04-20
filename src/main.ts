aa423e78984c84aceaa2ad5e3935ecd5fdeeacd3
Ludwring Liccien <ludwring.liccien@gmail.com>
2024-04-20 13:23:41 -0500

    feat(config): add module config for enviroment variable and path aliases in tsconfig

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from './config/services/config/config.service';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const logger = new Logger('NestApplication');
+
+  await app.listen(configService.get('PORT') || 3000, async () => {
+    logger.log(
+      `Server is running on http://localhost:${configService.get('PORT') || 3000}`,
+    );
+  });
 }
 bootstrap();
