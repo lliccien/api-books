4a79120dd1c5df7a6a7554781e42a855fc8faa70
Ludwring Liccien <ludwring.liccien@gmail.com>
2024-04-20 18:34:05 -0500

    feat(common): add module common

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 import { ProductsModule } from './products/products.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, ProductsModule],
+  imports: [ConfigModule, DatabaseModule, ProductsModule, CommonModule],
   controllers: [],
   providers: [],
 })
